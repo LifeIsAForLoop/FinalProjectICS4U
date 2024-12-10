afcdb0a256722dd0dfb1a52e7407ead458fb0c73
XaKit0340 <XaKit0340@48K2X52>
2024-12-10 14:55:52 -0500

    Started

---
+++ src/finalprojectics4u/FinalProjectICS4U.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package finalprojectics4u;
+
+/**
+ *
+ * @author XaKit0340
+ */
+public class FinalProjectICS4U {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
