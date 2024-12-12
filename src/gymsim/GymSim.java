61455bece82451837b01a6994b77d09cb4369ec6
xrkit <xrkit@DESKTOP-B45HEDK.home>
2024-12-12 00:05:40 -0500

    Started window

---
+++ src/gymsim/GymSim.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gymsim;
+
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import java.awt.Canvas;
+import javax.swing.*;
+
+/**
+ *
+ * @author XaKit0340
+ */
+/**
+ * @param args the command line arguments
+ */
+public class GymSim extends Canvas {
+    
+    
+    public static void main(String[] args) {
+        Display display = new Display(1000, 800);
+    }
+
+
+}
+
+
+
