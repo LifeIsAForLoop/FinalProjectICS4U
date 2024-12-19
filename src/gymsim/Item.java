f3b76d4146ae4a84b1b28c8f20a4c43f59633382
MaMey9786 <MaMey9786@3R9Y082>
2024-12-19 15:17:20 -0500

    Updated classes outlined on doc

--- src/gymsim/Item.java
+++ src/gymsim/Item.java
@@ -2,6 +2,46 @@
  */
 package gymsim;
 
-public class Item {
+abstract public class Item {
+    // Attributes 
+    Sprite sprite; 
+    String name; 
+    int boostMultiplier; 
+    // Constructors 
+    public Item() {
+        
+    }
+    
+    public Item(Sprite sprite, String name, int boostMultiplier) {
+        
+    }
+
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(Sprite sprite) {
+        this.sprite = sprite;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBoostMultiplier() {
+        return boostMultiplier;
+    }
+
+    public void setBoostMultiplier(int boostMultiplier) {
+        this.boostMultiplier = boostMultiplier;
+    }
+    
+    
+    
+    
     
 }
